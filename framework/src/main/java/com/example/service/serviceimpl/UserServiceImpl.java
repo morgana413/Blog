0308e4295abb5720c32e80bfe8b2ce283a53fd40
sino <2077450408@qq.com>
2025-05-29 14:21:22 +0800

    注册功能的实现

--- framework/src/main/java/com/example/service/serviceimpl/UserServiceImpl.java
+++ framework/src/main/java/com/example/service/serviceimpl/UserServiceImpl.java
@@ -1,14 +1,20 @@
 package com.example.service.serviceimpl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.domain.entity.ResponseResult;
 import com.example.domain.entity.User;
 import com.example.domain.vo.UserInfoVo;
+import com.example.enums.AppHttpCodeEnum;
+import com.example.exception.SystemException;
 import com.example.mapper.UserMapper;
 import com.example.service.UserService;
 import com.example.utils.BeanCopyUtils;
 import com.example.utils.SecurityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 /**
  * 用户表(User)表服务实现类
@@ -18,6 +24,9 @@ import org.springframework.stereotype.Service;
  */
 @Service("userService")
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public ResponseResult userInfo() {
@@ -34,5 +43,62 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
     public ResponseResult updateUserInfo(User user) {
         updateById(user);
         return ResponseResult.okResult();
+    }
+
+    @Override
+    public ResponseResult register(User user) {
+
+        //对数据进行非空判断
+        if(!StringUtils.hasText(user.getUserName())){
+            throw new SystemException(AppHttpCodeEnum.USERNAME_IS_NULL);
+        }
+
+        if(!StringUtils.hasText(user.getNickName())){
+            throw new SystemException(AppHttpCodeEnum.NICKNAME_IS_NULL);
+        }
+
+        if(!StringUtils.hasText(user.getPassword())){
+            throw new SystemException(AppHttpCodeEnum.PASSWORD_IS_NULL);
+        }
+
+        if (!StringUtils.hasText(user.getEmail())) {
+            throw new SystemException(AppHttpCodeEnum.MAIL_IS_NULL);
+        }
+        //验证数据是否唯一
+        if (userNameExist(user.getUserName())){
+            throw new SystemException(AppHttpCodeEnum.USERNAME_EXIST);
+        }
+
+        if (userMailExist(user.getEmail())){
+            throw new SystemException(AppHttpCodeEnum.EMAIL_EXIST);
+        }
+
+        if (userNicknamexist(user.getNickName())){
+            throw new SystemException(AppHttpCodeEnum.NICKNAME_EXIST);
+        }
+        //对密码进行加密
+        String password = passwordEncoder.encode(user.getPassword());
+        user.setPassword(password);
+        //存入数据库
+        save(user);
+        return ResponseResult.okResult();
+    }
+
+    private boolean userNicknamexist(String nickName) {
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(User::getNickName, nickName);
+        return count(queryWrapper) > 0;
+    }
+
+    private boolean userMailExist(String email) {
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(User::getEmail, email);
+        return count(queryWrapper) > 0;
+    }
+
+    private boolean userNameExist(String userName) {
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(User::getUserName, userName);
+        return count(queryWrapper) > 0;
     }
 }
